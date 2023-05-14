45102f9cd1e4d360cdb26d750f73241dc646f0a8
Kenyatta Clark <kenyatta.clark@gmail.com>
2023-05-14 01:16:07 -0500

    add logging configuration

--- data_stream.py
+++ data_stream.py
@@ -1,39 +1,32 @@
 import os
+import logging
+import csv
+import logging.config
+import yaml
 
 from alpaca.data.live import CryptoDataStream
-import csv
-
 from my_secrets import ALPACA_API_KEY, ALPACA_SECRET_KEY
 
-crypto_stream = CryptoDataStream(ALPACA_API_KEY, ALPACA_SECRET_KEY)
+with open('log_config.yaml', 'r') as log_config:
+    config = yaml.safe_load(log_config.read())
+    logging.config.dictConfig(config)
+
+logger = logging.getLogger(__name__)
 
 
 async def bar_callback(bar):
-    bar_tuple = bar_to_tuple(bar)
-    print(
-        bar_tuple
-    )
+    bar_tuple = [bar.symbol, bar.timestamp, bar.open, bar.high, bar.low, bar.close, bar.volume, bar.trade_count,
+                 bar.vwap]
 
-    # open the file in the write mode
-    # TODO read from a file or the environment
+    logging.debug(bar_tuple)
+
     f = open(os.path.join(os.getcwd(), 'out.csv'), 'a')
-
-    # create the csv writer
     writer = csv.writer(f)
-
-    # write a row to the csv file
     writer.writerow(bar_tuple)
-
-    # close the file
     f.close()
 
 
-def bar_to_tuple(bar):
-    return [bar.symbol, bar.timestamp, bar.open, bar.high, bar.low, bar.close, bar.volume, bar.trade_count, bar.vwap]
-
-
 # Subscribing to bar event
-symbol = "BTC/USD"
-crypto_stream.subscribe_bars(bar_callback, symbol)
-
+crypto_stream = CryptoDataStream(ALPACA_API_KEY, ALPACA_SECRET_KEY)
+crypto_stream.subscribe_bars(bar_callback, "BTC/USD")
 crypto_stream.run()
